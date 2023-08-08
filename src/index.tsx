97ff56a28cc2f5df7beedaa6740cf471a69ea64b
aidar <aydar.farhutdinov.123@gmail.com>
2023-08-08 13:43:49 +0500

    adas

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import ThemeProvider from "./theme/ThemeProvider";
+
+render(
+  <div>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </div>,
+  document.getElementById("root")
+);
